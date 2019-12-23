14da6e844d8a6b134ce31c0128a6b884c538132a
Alexander Maru <i@alexandermaru.com>
2019-12-23 00:17:06 -0500

    Abstract multiple criteria into a single function.

--- analyze.cc
+++ analyze.cc
@@ -22,35 +22,11 @@ bool consecutive(std::string target) {
 		target.find("CCCCCC") == std::string::npos;
 }
 
-// nucl1GorC(target) returns true if the 1st nucleotide of the 
-// target saRNA is either G or C, and false otherwise.
-bool nucl1GorC(std::string target) {
-	return target[0] == 'G' || target[0] == 'C';
-}
-
-// nucl2GorC(target) returns true if the 2nd nucleotide of the
-// target saRNA is either G or C, and false otherwise.
-bool nucl2GorC(std::string target) {
-	return target[1] == 'G' || target[1] == 'C';
-}
-
-// nucl18AorT(target) returns true if the 18th nucleotide of the
-// target saRNA is either A or T, and false otherwise.
-bool nucl18AorT(std::string target) {
-	return target[17] == 'A' || target[17] == 'T';
-}
-
-// nucl19A(target) returns true if the 19th nucleotide of the 
-// target saRNA is A, and false otherwise.
-bool nucl19A(std::string target) {
-	return target[18] == 'A';
-}
-
-// nucl2021AorT(next) returns true if "next" (which is either the
-// 20th or the 21st nucleotide) is either A or T,
-// and false otherwise.
-bool nucl2021AorT(char next) {
-	return next == 'A' || next == 'T';
+// isNucleotideXY(target, index, X, Y) returns true if the target saRNA
+// at the specified index contains either X or Y nucleotides, and false
+// otherwise.
+bool isNucleotideXY(std::string target, int index, char X, char Y) {
+	return target[index] == X || target[index] == Y;
 }
 
 // rnaIter(saRNA, targetSize) iterates over the saRNA string,
@@ -63,15 +39,16 @@ void rnaIter(std::string saRNA, unsigned int targetSize) {
 		
 		if (!gcContent(target)) continue;
 		if (!consecutive(target)) continue;
-		if (!nucl1GorC(target)) continue;
-		if (!nucl2GorC(target)) continue;
-		if (!nucl18AorT(target)) continue;
-		if (!nucl19A(target)) continue;
-		if (saRNA.size()-i >= targetSize+1) {
-			if (!nucl2021AorT(saRNA[i+targetSize+1])) continue;
-		}
+		if (!isNucleotideXY(target, 0, 'G', 'C')) continue;
+		if (!isNucleotideXY(target, 1, 'G', 'C')) continue;
+		if (!isNucleotideXY(target, 17, 'A', 'T')) continue;
+		if (!isNucleotideXY(target, 18, 'A', 'A')) continue;
+
 		if (saRNA.size()-i >= targetSize+2) {
-			if (!nucl2021AorT(saRNA[i+targetSize+2])) continue;
+			if (!isNucleotideXY(saRNA, i+targetSize+2, 'A', 'T')) continue;
+			if (!isNucleotideXY(saRNA, i+targetSize+1, 'A', 'T')) continue;
+		} else if (saRNA.size()-i >= targetSize+1) {
+			if (!isNucleotideXY(saRNA, i+targetSize+1, 'A', 'T')) continue;
 		}
 
 		std::cout << target << '\n';
